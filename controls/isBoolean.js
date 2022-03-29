5bb785fb5ca8b8d6fdb733a79c3e961c40fc9bbb
mehmetsagir <memetsagirint@gmail.com>
2022-03-30 02:18:25 +0300

    add isBoolean

---
+++ controls/isBoolean.js
@@ -0,0 +1,6 @@
+const isBoolean = (value) => {
+  const regex = /^([Tt][Rr][Uu][Ee]|[Ff][Aa][Ll][Ss][Ee])$/;
+  return regex.test(value) ? true : false;
+};
+
+module.exports = { isBoolean };
--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const { isNum } = require("./controls/isNum");
 const { isName } = require("./controls/isName");
 const { isImageURL } = require("./controls/isImageURL");
 const { isRegex } = require("./controls/isRegex");
+const { isBoolean } = require("./controls/isBoolean");
 
 module.exports = { isEmail, isNum, isName, isImageURL, isRegex };
