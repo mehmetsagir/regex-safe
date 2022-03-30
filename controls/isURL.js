60af158a357d26c6d529003ec396732a6d7606a4
mehmetsagir <memetsagirint@gmail.com>
2022-03-31 02:07:15 +0300

    add isURL

---
+++ controls/isURL.js
@@ -0,0 +1,8 @@
+const isURL = (url) => {
+  const regex =
+    /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g;
+
+  return regex.test(url) ? true : false;
+};
+
+module.exports = { isURL };
--- index.js
+++ index.js
@@ -4,5 +4,14 @@ const { isName } = require("./controls/isName");
 const { isImageURL } = require("./controls/isImageURL");
 const { isRegex } = require("./controls/isRegex");
 const { isBoolean } = require("./controls/isBoolean");
+const { isURL } = require("./controls/isURL");
 
-module.exports = { isEmail, isNum, isName, isImageURL, isRegex, isBoolean };
+module.exports = {
+  isEmail,
+  isNum,
+  isName,
+  isImageURL,
+  isRegex,
+  isBoolean,
+  isURL,
+};
